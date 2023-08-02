bc0820e7bb887c10912acc73f7bc3fcd6c1cda3d
Gabriel Barros <gabriel.barros@gbmlogistica.com.br>
2023-08-02 15:44:40 -0300

    criando txt

--- calendario/Program.cs
+++ calendario/Program.cs
@@ -38,6 +38,8 @@ namespace ConcatenarDiasDoAno
             }
 
             Console.WriteLine(calendario);
+
+            File.WriteAllText(@"C:\Users\Gabriel\Downloads\teste.txt", calendario);
         }
     }
 }
